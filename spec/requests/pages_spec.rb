012eca1dd5e0e30412682f920b91c5c4406a0374
noriko <nontaan55@yahoo.co.jp>
2021-05-26 00:50:07 +0900

    topページのrspecを追加

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
-  describe "GET /home" do
-    it "returns http success" do
-      get "/pages/home"
-      expect(response).to have_http_status(:success)
+  describe "GET /" do
+    it "httpリクエストが成功する" do
+      get root_path
+      expect(response).to have_http_status(200)
     end
   end
-
 end
