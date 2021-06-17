24a029dfe16fe6fcd32cfd8f9ed738a1679de68c
noriko <nontaan55@yahoo.co.jp>
2021-06-17 19:23:30 +0900

    ユーザープロフィールページのrspecを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
+
+    if current_user == @user
+      render 'users/show'
+    else
+      redirect_to root_url
+    end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
   factory :user do
+    sequence(:user_name) { |i| "User#{i}" }
+    sequence(:email) { |i| "email#{i}@example.com" }
+    password { "password" }
+    confirmed_at { Date.today }
   end
 end
