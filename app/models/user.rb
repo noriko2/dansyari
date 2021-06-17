f5bd0e6da5c6c3f95ca430988de538694d7557c0
noriko <nontaan55@yahoo.co.jp>
2021-06-17 19:18:37 +0900

    snsログインでは、メール認証を無効化

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,19 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     user = User.where(uid: auth.uid, provider: auth.provider).first
-    user ||= User.create(
-      uid: auth.uid,
-      provider: auth.provider,
-      email: auth.info.email,
-      user_name: auth.info.name,
-      password: Devise.friendly_token[0, 20],
-      profile_image: auth.info.image
-    )
+    unless user
+      user = User.new(
+        uid: auth.uid,
+        provider: auth.provider,
+        email: auth.info.email,
+        user_name: auth.info.name,
+        password: Devise.friendly_token[0, 20],
+        profile_image: auth.info.image
+      )
+      # メール認証をスキップ
+      user.skip_confirmation!
+      user.save
+    end
     user
   end
 
