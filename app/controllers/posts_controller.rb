2e08bc06eed04675195b6b63e7b1621302f038c9
noriko <nontaan55@yahoo.co.jp>
2021-06-29 17:57:36 +0900

    断捨離記録の削除機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
-  
+  before_action :set_post, only: [:show, :destroy]
+
   def new
     @post = Post.new
     @post.build_photo
@@ -19,19 +20,34 @@ class PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.limit(10).order('created_at DESC')
+    @posts = Post.where(user_id: current_user).limit(12).order('created_at DESC')
   end
 
   def show
-    @post = Post.find(params[:id].to_i)
     respond_to do |format|
       format.html
       format.js
     end
   end
 
+  def destroy
+    if @post.destroy
+      flash[:notice] = "記録が削除されました"
+    else
+      flash[:alert] = "記録の削除に失敗しました"
+    end
+    redirect_to posts_path
+  end
+
   private
     def post_params
       params.require(:post).permit(:caption, photo_attributes: [:image]).merge(user_id: current_user.id)
+    end
+
+    def set_post
+      @post = Post.find(params[:id].to_i)
+      if @post.user != current_user
+        redirect_to root_url
+      end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   }
   resources :users, only: [:show]
 
-  resources :posts, only: [:new, :create, :index, :show] do
+  resources :posts, only: [:new, :create, :index, :show, :destroy] do
     resources :photos, only: [:create]
   end
 end
