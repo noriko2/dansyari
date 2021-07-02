a35d9f234addbb60948cba96f7cbdaeea5ba06a7
noriko <nontaan55@yahoo.co.jp>
2021-07-02 17:43:45 +0900

    seedデータ(サンプルユーザー)を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+sample_user = User.create!(user_name: "Dansyari Dekiru",
+  email: "danysari@example.com",
+  password:              "dansyari",
+  password_confirmation: "dansyari",
+  # メール認証をスキップ (deviseのconfimableを確認済にする)
+  confirmed_at: Time.now
+)
+
+100.times do |n|
+  post = sample_user.posts.create!(
+    caption: "【 サンプル記録__#{n + 1} 】 ナイキスニーカー。靴の中に穴があいていたため断捨離。"
+  )
+  post.create_photo( image: File.open("./app/javascript/images/shoes.jpeg") )
+end
